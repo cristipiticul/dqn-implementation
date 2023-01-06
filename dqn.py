d464ebc7f1c03e38628291a5b5dea40ad059983b
Cristian Militaru <cristipiticul@yahoo.com>
2023-01-06 23:43:42 +0200

    A little refactoring, small work on ReplayMemory and tested. It crashes at epoch 4 at self.h5file.copy_file(dstfilename=filename). Will try compressing, chunking, h5py. Also the speed is terrible, maybe I can profile the times

--- dqn.py
+++ dqn.py
@@ -20,8 +20,17 @@ import os
 import csv
 import tables
 
+from typing import Dict
+
 REPLAY_MEMORY_FILE = "replay_memory.hdf5"
 REPLAY_MEMORY_TABLE_NAME = "replay_memory_table"
+
+
+criterion = torch.nn.MSELoss()
+gamma = 0.95  # TODO: choose a discount factor
+running_loss = 0
+num_epochs = 200
+replay_memory_size = 1000000
 
 
 class Net(nn.Module):
@@ -109,14 +118,21 @@ class ReplayMemoryRowDescription(tables.IsDescription):
 
 
 class ReplayMemory:
-    """Store multiple (prev_obs, action, reward, obs, done) in separate numpy arrays."""
+    """Store multiple (prev_obs, action, reward, obs, done) in a table using PyTables."""
 
-    def __init__(self, max_len, checkpoint_filename: str = None):
+    def __init__(
+        self,
+        max_len,
+        checkpoint_filename: str = None,
+        current_index_filename: str = None,
+    ):
         self.initialized = False
         self.max_len = max_len
-        self.open_file(checkpoint_filename)
+        self.open_file(checkpoint_filename, current_index_filename)
 
-    def open_file(self, checkpoint_filename: str = None):
+    def open_file(
+        self, checkpoint_filename: str = None, current_index_filename: str = None
+    ):
         if checkpoint_filename:
             tables.copy_file(checkpoint_filename, REPLAY_MEMORY_FILE, overwrite=True)
             self.h5file = tables.open_file(REPLAY_MEMORY_FILE, mode="a")
@@ -124,7 +140,7 @@ class ReplayMemory:
                 self.h5file.root, REPLAY_MEMORY_TABLE_NAME
             )
             self.n_items = self.table.nrows
-            with open(f"{checkpoint_filename}_index.txt", "r") as f:
+            with open(current_index_filename, "r") as f:
                 self.curr_index = int(f.read())
         else:
             self.h5file = tables.open_file(
@@ -178,9 +194,9 @@ class ReplayMemory:
     def get_minibatch(self, indices: list):
         return self.table.read_coordinates(indices)
 
-    def save(self, filename):
+    def save(self, filename, curr_index_filename):
         self.h5file.copy_file(dstfilename=filename)
-        with open(f"{filename}_index.txt", "w") as f:
+        with open(curr_index_filename, "w") as f:
             f.write(str(self.curr_index))
 
     def __len__(self):
@@ -188,14 +204,22 @@ class ReplayMemory:
 
 
 def replay_memory_test():
-    # TODO: test checkpoints
     print("Testing replay memory")
+    replay_memory_fields_test()
+    replay_memory_overflow_test()
+    replay_memory_checkpoint_test()
+    print("Done testing replay memory")
+
+
+def replay_memory_fields_test():
     r = ReplayMemory(max_len=4)
     prev_obs = np.zeros((4, 84, 84), dtype=np.float32)
     obs = np.zeros((4, 84, 84), dtype=np.float32)
     r.add(prev_obs, 0, 0.0, obs, False)
     r.add(prev_obs + 1, 1, 1.0, obs + 1, False)
     r.add(prev_obs + 2, 2, 2.0, obs + 2, True)
+    assert len(r) == 3
+    assert r.curr_index == 3
     mb = r.get_minibatch([0, 2])
     assert mb[0]["prev_obs"][0, 0, 0] == 0
     assert mb[0]["action"] == 0
@@ -207,24 +231,56 @@ def replay_memory_test():
     assert mb[1]["reward"] == 2
     assert mb[1]["obs"][0, 0, 0] == 2
     assert mb[1]["done"] == True
+    r.close()
+    os.remove(REPLAY_MEMORY_FILE)
 
-    # Fill completely and overflow
+
+def replay_memory_overflow_test():
+    r = ReplayMemory(max_len=4)
+    prev_obs = np.zeros((4, 84, 84), dtype=np.float32)
+    obs = np.zeros((4, 84, 84), dtype=np.float32)
+    r.add(prev_obs, 0, 0.0, obs, False)
+    r.add(prev_obs + 1, 1, 1.0, obs + 1, False)
+    r.add(prev_obs + 2, 2, 2.0, obs + 2, True)
     r.add(prev_obs + 3, 3, 3.0, obs + 3, True)
     r.add(prev_obs + 4, 4, 4.0, obs + 4, True)
     r.add(prev_obs + 5, 5, 5.0, obs + 5, True)
+    assert len(r) == 4
+    assert r.curr_index == 2
     mb = r.get_minibatch([0, 1, 2])
     assert mb[0]["prev_obs"][0, 0, 0] == 4
     assert mb[1]["prev_obs"][0, 0, 0] == 5
     assert mb[2]["prev_obs"][0, 0, 0] == 2
     r.close()
-    print("Done testing replay memory")
+    os.remove(REPLAY_MEMORY_FILE)
 
 
-criterion = torch.nn.MSELoss()
-gamma = 0.95  # TODO: choose a discount factor
-running_loss = 0
+def replay_memory_checkpoint_test():
+    r = ReplayMemory(max_len=4)
+    prev_obs = np.zeros((4, 84, 84), dtype=np.float32)
+    obs = np.zeros((4, 84, 84), dtype=np.float32)
+    r.add(prev_obs, 0, 0.0, obs, False)
+    r.add(prev_obs + 1, 1, 1.0, obs + 1, False)
+    r.add(prev_obs + 2, 2, 2.0, obs + 2, True)
+    r.add(prev_obs + 3, 3, 3.0, obs + 3, True)
+    r.add(prev_obs + 4, 4, 4.0, obs + 4, True)
+    r.add(prev_obs + 5, 5, 5.0, obs + 5, True)
+    r.save("test_checkpoint.hdf5", "test_checkpoint_index.txt")
+    r.close()
+    os.remove(REPLAY_MEMORY_FILE)
 
-from typing import Dict
+    r2 = ReplayMemory(4, "test_checkpoint.hdf5", "test_checkpoint_index.txt")
+    assert len(r2) == 4
+    assert r2.curr_index == 2
+    r2.add(prev_obs + 6, 6, 6.0, obs + 6, True)
+    mb = r2.get_minibatch([1, 2])
+    assert mb[0]["prev_obs"][0, 0, 0] == 5
+    assert mb[1]["prev_obs"][0, 0, 0] == 6
+    r2.close()
+
+    os.remove(REPLAY_MEMORY_FILE)
+    os.remove("test_checkpoint.hdf5")
+    os.remove("test_checkpoint_index.txt")
 
 
 def gradient_descent_step(
@@ -267,6 +323,32 @@ def gradient_descent_step(
     running_loss += loss.item()
 
 
+def model_checkpoint_filename(epoch):
+    return f"network_epoch_{epoch}.pt"
+
+
+def replay_memory_checkpoint_filename(epoch):
+    return f"replay_memory_epoch_{epoch}.hdf5"
+
+
+def replay_memory_index_checkpoint_filename(epoch):
+    return f"replay_memory_index_epoch_{epoch}.txt"
+
+
+def get_last_checkpoint():
+    latest_epoch_savepoint = None
+    for epoch in range(num_epochs):
+        if os.path.exists(model_checkpoint_filename(epoch)):
+            latest_epoch_savepoint = epoch
+    return latest_epoch_savepoint
+
+
+def load_checkpoint_model(trained_net, optimizer, epoch):
+    checkpoint = torch.load(model_checkpoint_filename(epoch))
+    trained_net.load_state_dict(checkpoint["model_state_dict"])
+    optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+
+
 def main():
     replay_memory_test()
     env = retro.make(game="BeamRider-Atari2600")
@@ -287,30 +369,26 @@ def main():
     optimizer = torch.optim.RMSprop(trained_net.parameters())
 
     # Find latest savepoint:
-    latest_epoch_savepoint = -1
-    for epoch in range(200):
-        if os.path.exists(f"./network_epoch_{epoch}.pt"):
-            latest_epoch_savepoint = epoch
-    if latest_epoch_savepoint >= 0:
-        print(f"Latest checkpoint found: epoch {latest_epoch_savepoint}")
-        checkpoint = torch.load(f"./network_epoch_{latest_epoch_savepoint}.pt")
-        trained_net.load_state_dict(checkpoint["model_state_dict"])
-        optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+    last_checkpoint_epoch = get_last_checkpoint()
+    if last_checkpoint_epoch:
+        print(f"Latest checkpoint found: epoch {last_checkpoint_epoch}")
+        load_checkpoint_model(trained_net, optimizer, last_checkpoint_epoch)
+        replay_memory = ReplayMemory(
+            replay_memory_size,
+            replay_memory_checkpoint_filename(last_checkpoint_epoch),
+            replay_memory_index_checkpoint_filename(last_checkpoint_epoch),
+        )
     else:
         print("No checkpoint found, starting from beginning")
+        replay_memory = ReplayMemory(replay_memory_size)
 
     prev_obs = env.reset()
-    replay_memory_filename = None
-    if latest_epoch_savepoint >= 0:
-        replay_memory_filename = f"./replay_memory_epoch_{latest_epoch_savepoint}.hdf5"
-    replay_memory = ReplayMemory(
-        max_len=1000000, checkpoint_filename=replay_memory_filename
-    )
     minibatch_size = 32
     frame_index = 0
     with open("log.csv", "a") as file:
         writer = csv.writer(file)
-        for epoch in range(latest_epoch_savepoint + 1, 200):
+        start_epoch = (last_checkpoint_epoch + 1) if last_checkpoint_epoch else 0
+        for epoch in range(start_epoch, num_epochs):
             net_updates_count = 0
             prev_net.load_state_dict(trained_net.state_dict())
             # One epoch corresponds to 50000 minibatch weight updates
@@ -378,9 +456,12 @@ def main():
                     "optimizer_state_dict": optimizer.state_dict(),
                     # 'loss': LOSS,
                 },
-                f"./network_epoch_{epoch}.pt",
+                model_checkpoint_filename(epoch),
             )
-            replay_memory.save(f"./replay_memory_epoch_{epoch}.hdf5")
+            replay_memory.save(
+                replay_memory_checkpoint_filename(epoch),
+                replay_memory_index_checkpoint_filename(epoch),
+            )
     env.close()
 
 
